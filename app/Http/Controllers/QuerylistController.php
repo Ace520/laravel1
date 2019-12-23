295bb012cd3595280dda20916ce9de45cfbc21e0
Ace <ace963ace@gmail.com>
2019-12-23 18:49:30 +0800

    html-to-markdown table

--- app/Http/Controllers/QuerylistController.php
+++ app/Http/Controllers/QuerylistController.php
@@ -4,21 +4,57 @@
 namespace App\Http\Controllers;
 
 
+use App\Libs\Converter\TableConverter;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use League\HTMLToMarkdown\Converter\BlockquoteConverter;
+use League\HTMLToMarkdown\Converter\CodeConverter;
+use League\HTMLToMarkdown\Converter\CommentConverter;
+use League\HTMLToMarkdown\Converter\DefaultConverter;
+use League\HTMLToMarkdown\Converter\EmphasisConverter;
+use League\HTMLToMarkdown\Converter\HardBreakConverter;
+use League\HTMLToMarkdown\Converter\HeaderConverter;
+use League\HTMLToMarkdown\Converter\HorizontalRuleConverter;
+use League\HTMLToMarkdown\Converter\ImageConverter;
+use League\HTMLToMarkdown\Converter\LinkConverter;
+use League\HTMLToMarkdown\Converter\ListBlockConverter;
+use League\HTMLToMarkdown\Converter\ListItemConverter;
+use League\HTMLToMarkdown\Converter\ParagraphConverter;
+use League\HTMLToMarkdown\Converter\PreformattedConverter;
+use League\HTMLToMarkdown\Converter\TextConverter;
+use League\HTMLToMarkdown\Environment;
 use League\HTMLToMarkdown\HtmlConverter;
 use QL\QueryList;
 
 class QuerylistController extends Controller
 {
     public function index(){
-        $url = 'https://learnku.com/articles/38375';
+        $url = 'https://learnku.com/laravel/t/5420/your-keyboard-shortcuts-please';
         if (!$html = Cache::get($url)){
             $html = QueryList::get($url)->find('.markdown-body')->html();
             Cache::forever($url,$html);
         }
-        $converter = new HtmlConverter(['header_style' => 'atx']);
-        $converter->getConfig()->setOption('list_item_style', '*');
+        $environment = new Environment(array(
+            'preserve_comments' => true
+        ));
+        $environment->addConverter(new BlockquoteConverter());
+        $environment->addConverter(new CodeConverter());
+        $environment->addConverter(new CommentConverter());
+        $environment->addConverter(new DefaultConverter());
+        $environment->addConverter(new EmphasisConverter());
+        $environment->addConverter(new HardBreakConverter());
+        $environment->addConverter(new HeaderConverter());
+        $environment->addConverter(new HorizontalRuleConverter());
+        $environment->addConverter(new ImageConverter());
+        $environment->addConverter(new LinkConverter());
+        $environment->addConverter(new ListBlockConverter());
+        $environment->addConverter(new ListItemConverter());
+        $environment->addConverter(new ParagraphConverter());
+        $environment->addConverter(new PreformattedConverter());
+        $environment->addConverter(new TextConverter());
+        $environment->addConverter(new TableConverter());
+        $converter = new HtmlConverter($environment);
         $markdown = $converter->convert($html);
-        print_r($markdown);
+        Log::info($markdown);
     }
 }
